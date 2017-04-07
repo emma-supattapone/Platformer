536b8857b6ab5dfe3b7a36ccc81df6de60cd5bfe
emma-supattapone <emmasupattapone@hanovernorwichschools.org>
2017-04-07 14:53:31 -0400

    Updated from Brython Server: 4/7/2017 2:53:50 PM

--- platformer.py
+++ platformer.py
@@ -6,3 +6,14 @@ Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
+from ggame import App
+myapp = App()
+myapp.run()
+
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
